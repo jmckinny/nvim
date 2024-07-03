0eb016272788a657efbfdfaaeefd68f93064659e
Justin McKinney <justinmckinney.md@gmail.com>
2024-07-03 17:20:50 -0400

    inital config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<leader>q", vim.cmd.quit, { desc = "Quit file", silent = true })
+map("n", "<leader>Q", vim.cmd.only, { desc = " Quit all", silent = true })
