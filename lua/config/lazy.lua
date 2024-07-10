c56ed06ecb58dc6760d7439ef92a6bfdd2ef49a7
Justin McKinney <justinmckinney.md@gmail.com>
2024-07-09 20:45:01 -0400

    feat: add toml

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     -- formatters/linters
     { import = "lazyvim.plugins.extras.formatting.prettier" },
