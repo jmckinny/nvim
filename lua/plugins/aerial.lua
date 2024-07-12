92353920caef101595c0b9a3d1550777339848d5
Justin McKinney <justinmckinney.md@gmail.com>
2024-07-11 21:18:26 -0400

    feat: add aerial conf

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,9 @@
+return {
+  { import = "lazyvim.plugins.extras.editor.aerial" },
+  {
+    "stevearc/aerial.nvim",
+    keys = {
+      { "<leader>a", "<cmd>AerialToggle<cr>", desc = "Aerial (Symbols)" }, -- a is for aerial!
+    },
+  },
+}
