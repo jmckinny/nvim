f698cdb1a02140b6cce277308e6c71f05b42ee6d
Justin McKinney <justinmckinney.md@gmail.com>
2024-07-11 21:17:57 -0400

    feat: more colorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,11 @@
+return {
+  { "rebelot/kanagawa.nvim", lazy = false },
+  { "folke/tokyonight.nvim", lazy = false },
+  { "navarasu/onedark.nvim", lazy = false },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
