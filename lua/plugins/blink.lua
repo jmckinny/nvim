84901f6197b087326faa133207967a3dc5eb3803
Justin McKinney <justinmckinney.md@gmail.com>
2024-12-16 19:03:49 -0500

    fix: use supertab + enter

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,8 @@
+return {
+  "saghen/blink.cmp",
+  lazy = false,
+  opts = {
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    keymap = { preset = "enter" },
+  },
+}
--- lua/plugins/supertab.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "saghen/blink.cmp",
-  lazy = false,
-  opts = {
-    keymap = { preset = "super-tab" },
-  },
-}
