7f2cb737ee678ab3a8c22843ad9d53eb991d7a7a
Justin McKinney <justinmckinney.md@gmail.com>
2024-12-16 19:08:29 -0500

    chore: add options list

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,6 +2,8 @@ return {
   "saghen/blink.cmp",
   lazy = false,
   opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     keymap = { preset = "enter" },
   },
