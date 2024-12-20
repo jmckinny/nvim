647a68379f9472407bfaf3c9ab860f85eade8e33
Justin McKinney <justinmckinney.md@gmail.com>
2024-12-20 10:31:44 -0500

    fix: allow enter confirm for blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,6 +5,6 @@ return {
     -- 'default' for mappings similar to built-in completion
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    keymap = { preset = "enter" },
+    keymap = { preset = "super-tab", ["<CR>"] = { "accept", "fallback" } },
   },
 }
