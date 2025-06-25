e8acf440ce18968d2407df4e24d00dd4a1c91ddc
Justin McKinney <justinmckinney.md@gmail.com>
2025-06-24 21:36:20 -0400

    fix: manually set super-tab config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,9 +2,33 @@ return {
   "saghen/blink.cmp",
   lazy = false,
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    keymap = { preset = "super-tab", ["<CR>"] = { "accept", "fallback" } },
+    keymap = {
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+      ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+    },
   },
 }
